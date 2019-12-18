ee969271cdd584560361154fd913b45fbdc1ed62
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-18 15:56:32 -0600

    mvp met

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const server = require("./api/server");
 
 const port = process.env.PORT || 4000;
 
-server.listen(port, () => console.log(`\n>>> Server is listening on port ${port}! <<<\n`)); 
+server.listen(port, () => console.log(`\n>>> Server is listening on port ${port}! <<<\n`));
